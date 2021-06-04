535047d5a642846fd291061874eb1cc6d02f7858
Diachenko, Oleksandr <oleksandr.diachenko@epam.com>
2021-06-04 12:57:54 +0300

    Created project

---
+++ src/test/java/com/epam/rgg/MainPageTest.java
@@ -0,0 +1,55 @@
+package com.epam.rgg;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainPageTest {
+    MainPage mainPage = new MainPage();
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1280x800";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("https://www.jetbrains.com/");
+    }
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        $("#header-search").sendKeys("Selenium");
+        $x("//button[@type='submit' and text()='Search']").click();
+
+        $(".js-search-input").shouldHave(attribute("value", "Selenium"));
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.hover();
+
+        $(".menu-main__popup-wrapper").shouldBe(visible);
+    }
+
+    @Test
+    public void navigationToAllTools() {
+        mainPage.seeAllToolsButton.click();
+
+        $(".products-list").shouldBe(visible);
+
+        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+    }
+}
